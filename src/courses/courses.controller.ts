f77d469a61fad63739e263889c7f967a883f2f85
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-06-30 06:45:25 +0100

    Create Courses controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,12 @@
+import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+	@Get()
+	findAll() {
+		return {
+			courses: []
+		};
+	}
+}
