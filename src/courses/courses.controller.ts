6dee7a7e938aa84712f521e0dbe41568baf01bb3
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-06-30 07:23:41 +0100

    Using http status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
 	@Get()
-	findAll() {
-		return {
+	findAll(@Res() response) {
+		return response.status(200).send({
 			courses: []
-		};
+		});
 	}
 
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return {
 			course: id
+		};
+	}
+
+	@Post()
+	create(@Body() body) {
+		return {
+			data: body
 		};
 	}
 }
