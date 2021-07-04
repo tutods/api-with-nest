3810ee5dc892dd855620ca2c50823f7e53b45f2c
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-07-04 21:33:21 +0100

    Add validations pipes

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateCourseDto {
+	@IsString({ message: 'Name needs to be a string!' })
 	readonly name: string;
+
+	@IsString()
 	readonly description: string;
+
+	@IsString({ each: true, message: 'Each tags needs to be a string' })
 	readonly tags: string[];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	await app
+		// Pipes
+		.useGlobalPipes(new ValidationPipe())
+		// Port
+		.listen(3000);
 }
 bootstrap();
