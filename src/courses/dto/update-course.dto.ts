484a6214714f8c8d034af8ca42eb3fdcfb83ee16
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-07-04 21:33:47 +0100

    Add validations pipes

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class UpdateCourseDto {
+	@IsString()
 	readonly name?: string;
+
+	@IsString()
 	readonly description?: string;
+
+	@IsString({ each: true, message: 'Each tags needs to be a string' })
 	readonly tags?: string[];
 }
